71932df69cfce3a6a7ae74bc198720e102ed0724
Louis M Privette III <louis.privette@gmail.com>
2018-08-03 18:19:35 -0400

    Fuctional, nearly complete. Few To Do items left.

--- src/co/grandcircus/fbjava_july2018/lab_08/Lab_08.java
+++ src/co/grandcircus/fbjava_july2018/lab_08/Lab_08.java
@@ -1,6 +1,7 @@
 package co.grandcircus.fbjava_july2018.lab_08;
 
 import java.util.Scanner;
+//import java.util.regex.Matcher;
 
 public class Lab_08 {
 	public static void main(String[] args) {
@@ -8,25 +9,20 @@ public class Lab_08 {
 	// Task: Write a program that will recognize invalid inputs when the
 	// user requests information about students in a class.
 
-// Characters from the Brad Neely web short The Professor Brothers, later made into as series called China, IL.		
-//					
-//		Name				Role							Connection to other characters 
-//	Baby Cakes			Student							son of P. Cakes
-//	Crystal Peppers 	history teacher					rival of Steve
-//	The Dean			School Dean  					rival of the mayor
-//	Frank Smith			history teacher					brother of Steve
-//	Professor Cakes		teacher of \'super science\'	father of b cakes
-//	Sammy Davis			history teacher					oldest staff
-//	Steve Smith			history teacher					brother of frank
-//	Pony Merks			teacher assistant of history	youngest staff
-//	The Mayor			City Mayor						rival of the dean
+// Characters from the Brad Neely web short The Professor Brothers, later made into as series called China, IL.
 	
-		Scanner scnr = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		
-		String characterName;
+		//String characterName;
+		String prompt;
+		String regex = "(roles|connection)";
+		String yeaNay;
 		int characterNum;
+		int min = 1;
+		int max = 9;
+		//regex = ?
 
-		String[] characters = new String[8];
+		String[] characters = new String[9];
 		characters[0] = "Baby Cakes";
 		characters[1] = "Crystal Peppers";
 		characters[2] = "The Dean";
@@ -37,18 +33,18 @@ public class Lab_08 {
 		characters[7] = "Sammy Davis";
 		characters[8] = "Steve Smith";
 		
-		String[] roles = new String[8];
+		String[] roles = new String[9];
 		roles[0] = "Student";
 		roles[1] = "History Teacher";
 		roles[2] = "School Dean";
 		roles[3] = "History Teacher";
 		roles[4] = "Town Mayor";
-		roles[5] = "Teacher of 'Super Science'";
+		roles[5] = "Teacher of \"Super Science\"";
 		roles[6] = "History Teacher Assistant";
 		roles[7] = "History Teacher";
 		roles[8] = "History Teacher";
 		
-		String[] connection = new String[8];
+		String[] connection = new String[9];
 		connection[0] = "Son of Professor Cakes";
 		connection[1] = "Rival of Steve Smith";
 		connection[2] = "Rival of The Mayor";
@@ -59,12 +55,34 @@ public class Lab_08 {
 		connection[7] = "Oldest Faculty Member";
 		connection[8] = "Brother of Frank";
 		
-		System.out.println("Welcome to the Worst College in America! Which character would you like to learn "
-				+ "more about? (enter a number 1-9): ");
+		do {
 		
-		System.out.println("That character does not exist or unrepresented here. Please try again. (enter a"
-				+ " number 1-9): ");
+		prompt = "Welcome to the Worst College in America! Which character would you like to learn more about?"
+						+ " (enter a number 1-9): ";
+
+		characterNum = Validation.getInt(input, prompt, min, max);
 		
-		System.out.println("Character " + characterNum  + " is " + characterName);
+			System.out.println("You have chosen the character " + characters[characterNum-1] + ". What would you like to know"
+					+ " more about " + characters[characterNum-1] + "? (enter \"roles\" or \"connection\"): ");
+		
+			String userChoice = Validation.getStringMatchingRegex(input, "", regex);
+			
+			if (userChoice.equalsIgnoreCase("roles")) {
+				System.out.println("You have chosen to know what " + characters + " role a and they " + roles);
+			}
+			
+			if (userChoice.equalsIgnoreCase("connection")) {
+				System.out.println(connection[characterNum]);
+			}
+			//TODO logic to allow user to chose opposite  
+		System.out.println("");
+		System.out.print("Do you want to continue (y/n)? ");
+		yeaNay = input.next();
+		
+		} while (yeaNay.equals("y"));	
+		
+		System.out.println("Goodbye.");
+		
+		input.close();
 	}
 }
